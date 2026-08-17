1c87616bd5287f924880f877ff4a543673501b00
michal102 <michal102xx@gmail.com>
2026-08-17 21:40:49 +0200

    changed allowed hosts

--- FridgeProject/settings.py
+++ FridgeProject/settings.py
@@ -30,9 +30,7 @@ SECRET_KEY = 'django-insecure-j4@p==4$^b_@kjwapzy*8)lo%&82xjyxiw4dlb1(g==en$7f&7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["fridgeproject-0tjd.onrender.com",
-                 "localhost"]
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
