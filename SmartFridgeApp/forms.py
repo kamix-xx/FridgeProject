cfb56c35c42b8f2d1041a743a6865e95fa896bb2
PawelSwieca <pawel.swieca@wp.pl>
2026-08-20 13:11:22 +0200

    Nowa migracja

--- SmartFridgeApp/forms.py
+++ SmartFridgeApp/forms.py
@@ -11,4 +11,4 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'email', 'first_name')
+        fields = ('login', 'email', 'first_name')
