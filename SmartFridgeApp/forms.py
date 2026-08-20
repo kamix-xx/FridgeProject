1afbbf9bfeaf5b9f317e2f480c58c4306d699530
kamix-xx <kamiltokarski120@gmail.com>
2026-08-20 12:59:44 +0200

    custom user model

--- SmartFridgeApp/forms.py
+++ SmartFridgeApp/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+User = get_user_model()
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
