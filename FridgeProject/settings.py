4fcdbc96a52a1f91d99adfa70a442a84720a6c39
kamix-xx <kamiltokarski120@gmail.com>
2026-08-20 12:00:13 +0200

    register

--- FridgeProject/settings.py
+++ FridgeProject/settings.py
@@ -20,7 +20,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
@@ -73,8 +72,10 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'FridgeProject.wsgi.application'
+LOGIN_REDIRECT_URL = 'home'  # Nazwa widoku/URL po zalogowaniu
+LOGOUT_REDIRECT_URL = 'login'  # Nazwa widoku/URL po wylogowaniu
 
+WSGI_APPLICATION = 'FridgeProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
@@ -86,7 +87,6 @@ DATABASES = {
         conn_health_checks=True,
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
@@ -106,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
@@ -117,7 +116,6 @@ TIME_ZONE = 'Europe/Warsaw'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
